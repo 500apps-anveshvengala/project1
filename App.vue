66d5e8d08be3665482e1234b80d9134bf0bf7c50
Vengala Anvesh <89439214+anvesh119@users.noreply.github.com>
2021-08-26 10:31:20 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  <h1>{{ title }}</h1>
+  <p>{{ greeting() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      title:'Hello Anvesh'
+    }
+  },
+  methods:{
+    greeting:function(){
+      return 'Hellooooooooo';
+    }
+  }
+}
+  
+  
+</script>
+
